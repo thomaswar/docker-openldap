12d8e269025837c0517ec85dfae149136705b076
r2h2 <rainer@hoerbe.at>
2016-03-10 22:28:23 +0100

    set default password for test user

--- resources/scripts/init_sample_data_wpvAt.sh
+++ resources/scripts/init_sample_data_wpvAt.sh
@@ -5,3 +5,9 @@ echo "loading /etc/openldap with sample data"
 ldapadd -h localhost -p $SLAPDPORT \
     -x -D cn=admin,dc=at -w $ROOTPW \
     -f /opt/sample_data/etc/openldap/data/wpvAt_init.ldif
+
+ldappasswd -h localhost -p $SLAPDPORT \
+    -x -D cn=admin,dc=at -w $ROOTPW \
+    -s 'test' \
+    'uid=tester@testinetics.at, gln=9110017333914, dc=wpv, dc=at'
+
