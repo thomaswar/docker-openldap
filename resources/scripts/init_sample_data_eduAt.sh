f944b7d8c7eed6512a8b1a6ea2c6a86ba5ae0a4a
r2h2 <rainer@hoerbe.at>
2016-06-07 22:06:10 +0200

    updated for edu data

---
+++ resources/scripts/init_sample_data_eduAt.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "loading openldap with sample data"
+
+ldapadd -h localhost -p $SLAPDPORT \
+    -x -D cn=admin,dc=at -w $ROOTPW \
+    -f /opt/sample_data/etc/openldap/data/eduAt_init.ldif
+
+ldappasswd -h localhost -p $SLAPDPORT \
+    -x -D cn=admin,dc=at -w $ROOTPW \
+    -s 'test' \
+    'uid=tester@testinetics.at, dc=edu, dc=at'
+
