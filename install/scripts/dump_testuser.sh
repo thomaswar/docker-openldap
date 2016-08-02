6c302790583230708b2e3e30901141a4a24a14e9
r2h2 <rainer@hoerbe.at>
2016-08-02 19:56:06 +0200

    add query for eduAt

--- install/scripts/dump_testuser.sh
+++ install/scripts/dump_testuser.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ldapsearch -h localhost -p $SLAPDPORT -x -D cn=admin,dc=at -w $ROOTPW -b dc=at -L 'uid=tester@testinetics.at'
+ldapsearch -h localhost -p $SLAPDPORT -x -D cn=admin,dc=at -w $ROOTPW -b dc=at -L 'uid=test.user1234567'
 
 # ldapmodify -h localhost -p $SLAPDPORT -x -D cn=admin,dc=at -w $ROOTPW -f /tmp/x.ldif
 
