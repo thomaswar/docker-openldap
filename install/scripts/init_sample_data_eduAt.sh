f438527294e9fe8af5a48c1463aef1d4a7d1de99
r2h2 <rainer@hoerbe.at>
2016-08-25 19:02:03 +0200

    make edu.at and ldapgvat schemas co-exist

--- install/scripts/init_sample_data_eduAt.sh
+++ install/scripts/init_sample_data_eduAt.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "loading openldap with sample data"
+echo "loading openldap with sample edu data"
 
 ldapadd -h localhost -p $SLAPDPORT \
     -x -D cn=admin,dc=at -w $ROOTPW \
@@ -9,6 +9,15 @@ ldapadd -h localhost -p $SLAPDPORT \
 ldappasswd -h localhost -p $SLAPDPORT \
     -x -D cn=admin,dc=at -w $ROOTPW \
     -s 'test' 'uid=test.user1234567, dc=schule, dc=at'
+
+#echo "loading openldap with sample gv.at data"
+#ldapadd -h localhost -p $SLAPDPORT \
+#    -x -D cn=admin,dc=at -w $ROOTPW \
+#    -c -f /opt/sample_data/etc/openldap/data/eduAt_init.ldif
+#
+#ldappasswd -h localhost -p $SLAPDPORT \
+#    -x -D cn=admin,dc=at -w $ROOTPW \
+#    -s 'test' 'gvGid=AT:B:1:12345,ou=people,gvOuId=AT:TEST:1,dc=gv,dc=at'
 
 # to undo:
 # ldapdelete -h localhost -p $SLAPDPORT -x -D cn=admin,dc=at -w $ROOTPW 'uid=test.user1234567,dc=schule,dc=at'
