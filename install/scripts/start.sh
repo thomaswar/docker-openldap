97fa08fbd02aa2fd8ca4d7f664f4f48b0476b554
r2h2 <rainer@hoerbe.at>
2017-03-26 21:11:20 +0200

    fix start

--- install/scripts/start.sh
+++ install/scripts/start.sh
@@ -5,6 +5,4 @@
 cp /etc/hosts /tmp/hosts
 sed -e "s/$HOSTNAME\$/$HOSTNAME $SLAPDHOST/" /tmp/hosts > /etc/hosts
 
-su - ldap -c slapd -4 -h "ldap://$SLAPDHOST:$SLAPDPORT/" \
-    -d conns,config,stats,shell,trace \
-    -f /etc/openldap/slapd.conf
+su - ldap -c "slapd -4 -h ldap://$SLAPDHOST:$SLAPDPORT/ -d conns,config,stats,shell,trace -f /etc/openldap/slapd.conf"
