6e2d6f32955f3eef1343acba7b562536cbc48d01
r2h2 <rainer@hoerbe.at>
2017-05-12 14:14:35 +0200

    fix connection setup

--- install/tests/init_sample_config_edu_at.sh
+++ install/tests/init_sample_config_edu_at.sh
@@ -8,7 +8,7 @@ if [ $(grep -q '^rootpw' /etc/openldap/slapd.conf) ]; then
     echo "rootpw directive already set in slapd.conf"
 else
     slappasswd -s $ROOTPW > /tmp/rootpw
-    echo "rootpw $(cat /tmp/rootpw)" >> /etc/openldap/slapd.conf
+    printf "\nrootpw $(cat /tmp/rootpw)" >> /etc/openldap/slapd.conf
     rm -f /tmp/rootpw
     echo "rootpw directive added to slapd.conf"
 fi
