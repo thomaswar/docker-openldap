cabb6347b8493201bb4509367f225979e237686e
r2h2 <rainer@hoerbe.at>
2017-06-06 17:07:59 +0200

    add test data

--- install/tests/dump_testuser.sh
+++ install/tests/dump_testuser.sh
@@ -2,6 +2,6 @@
 
 ldapsearch -h localhost -p $SLAPDPORT -x -D cn=admin,dc=at -w $ROOTPW -b dc=at -L 'cn=*'
 
-#ldapsearch -h localhost -p $SLAPDPORT -x -D cn=admin,dc=at -w $ROOTPW -b dc=at -L 'uid=test.user1234567'
-# ldapmodify -h localhost -p $SLAPDPORT -x -D cn=admin,dc=at -w $ROOTPW -f /tmp/x.ldif
-
+# ldapsearch -h localhost -p $SLAPDPORT -x -D cn=admin,dc=at -w $ROOTPW -b dc=at -L 'uid=test.user1234567'
+# ldapmodify -h localhost -p $SLAPDPORT -x -D cn=admin,dc=at -w $ROOTPW -c -f /tmp/x.ldif
+# ldapdelete -h localhost -p $SLAPDPORT -x -D cn=admin,dc=at -w $ROOTPW uid=test.user2_adam,dc=schule,dc=at
--- install/tests/init_sample_data_gvAt.sh
+++ install/tests/init_sample_data_gvAt.sh
@@ -5,3 +5,15 @@ echo "loading /etc/openldap with sample data "
 ldapadd -h localhost -p $SLAPDPORT \
     -x -D cn=admin,dc=at -w $ROOTPW \
     -f /opt/sample_data/etc/openldap/data/gvAt_init.ldif
+
+ldappasswd -h localhost -p $SLAPDPORT \
+    -x -D cn=admin,dc=at -w $ROOTPW \
+    -s 'test' 'gvGid=AT:B:1:12346,ou=people,gvOuId=AT:TEST:1,dc=gv,dc=at'
+
+ldappasswd -h localhost -p $SLAPDPORT \
+    -x -D cn=admin,dc=at -w $ROOTPW \
+    -s 'test' 'gvGid=AT:B:1:12347,ou=people,gvOuId=AT:TEST:1,dc=gv,dc=at'
+
+ldappasswd -h localhost -p $SLAPDPORT \
+    -x -D cn=admin,dc=at -w $ROOTPW \
+    -s 'test' 'gvGid=AT:B:1:12348,ou=people,gvOuId=AT:TEST:1,dc=gv,dc=at'
