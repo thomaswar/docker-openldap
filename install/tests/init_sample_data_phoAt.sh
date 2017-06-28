10cce9f945ee6b7889e8b3a673b183ce6aca6397
war <thomas@warwaris.at>
2017-06-28 20:35:31 +0200

    host from $SLAPDHOST

--- install/tests/init_sample_data_phoAt.sh
+++ install/tests/init_sample_data_phoAt.sh
@@ -2,11 +2,11 @@
 
 echo "loading /etc/openldap with sample data "
 
-ldapadd -h localhost -p $SLAPDPORT \
+ldapadd -h $SLAPDHOST -p $SLAPDPORT \
     -x -D cn=admin,dc=at -w $ROOTPW \
     -c -f /opt/sample_data/etc/openldap/data/phoAt_init.ldif
 
-ldappasswd -h localhost -p $SLAPDPORT \
+ldappasswd -h $SLAPDHOST -p $SLAPDPORT \
     -x -D cn=admin,dc=at -w $ROOTPW \
     -s 'test' \
     'cn=test.user1234567, o=ph-noe, dc=ac, dc=at'
