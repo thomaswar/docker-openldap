e6f33f83c6f711359a851416ba05c00eff23eeed
war <thomas@warwaris.at>
2017-06-28 21:40:52 +0200

    use a predefined hosts file

--- install/scripts/start.sh
+++ install/scripts/start.sh
@@ -6,9 +6,9 @@ cp /etc/hosts /tmp/hosts
 # maybe there is a prepared hosts file
 if [ -e /etc/openldap/slapd_hosts ] ; then
 	cp /etc/openldap/slapd_hosts /etc/hosts
+else
+	# add SLAPDHOST to /etc/hosts
+	sed -e "s/$HOSTNAME\$/$HOSTNAME $SLAPDHOST/" /tmp/hosts > /etc/hosts
 fi
-
-# add SLAPDHOST to /etc/hosts
-sed -e "s/$HOSTNAME\$/$HOSTNAME $SLAPDHOST/" /tmp/hosts > /etc/hosts
 
 su - ldap -c "slapd -4 -h ldap://$SLAPDHOST:$SLAPDPORT/ -d conns,config,stats,shell,trace -f /etc/openldap/slapd.conf"
